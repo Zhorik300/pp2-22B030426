77d35fbab8f6894253279b4aa880aeb482a10e2c
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-03-09 20:44:36 +0300

    Update 4.py

--- tsis5/Regex.md/4.py
+++ tsis5/Regex.md/4.py
@@ -1,9 +1,8 @@
-import re
-def text_match(text):
-        patterns = '[A-Z]+[a-z]+$'
-        if re.search(patterns, text):
-                return 'Found a match!'
-        else:
-                return('Not matched!')
-a = input()
-print(text_match(a))
+import re 
+p=str(input())
+x=re.findall("[A-Z]+[a-z]+$",p)
+print(x)
+if x:
+    print("Yes")
+else:
+    print("No")
