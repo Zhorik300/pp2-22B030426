ba433604ae23496cac03db449d370c8663628ddb
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-03-09 20:39:52 +0300

    Update 10.py

--- tsis5/Regex.md/10.py
+++ tsis5/Regex.md/10.py
@@ -1,6 +1,8 @@
-def camel_to_snake(text):
-        import re
-        str1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
-        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', str1).lower()
-a = input()
-print(camel_to_snake(a))
+import re
+p=str(input())
+x=re.findall("ab{2,3}",p)
+print(x)
+if x:
+    print("Yes")
+else:
+    print("No")
