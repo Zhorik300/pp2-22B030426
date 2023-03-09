7de71aa37c704d68de915564ef46b945ad44cdbb
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-03-09 20:44:07 +0300

    Update 3.py

--- tsis5/Regex.md/3.py
+++ tsis5/Regex.md/3.py
@@ -1,10 +1,8 @@
-import re
-def text_match(text):
-        patterns = '^[a-z]+_[a-z]+$'
-        if re.search(patterns,  text):
-                return 'Found a match!'
-        else:
-                return('Not matched!')
-
-a = input()
-print(text_match(a))
+import re 
+p=str(input())
+x=re.findall("\w",p)
+print(x)
+if x:
+    print("Yes")
+else:
+    print("No")
