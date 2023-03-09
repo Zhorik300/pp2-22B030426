c0f0c5fe900bc9d1e7f3fc8adaffad39d28328c6
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-03-09 20:39:05 +0300

    Update 1.py

--- tsis5/Regex.md/1.py
+++ tsis5/Regex.md/1.py
@@ -1,10 +1,9 @@
 
 import re
-def text_match(text):
-        patterns = '^a(b*)$'
-        if re.search(patterns,  text):
-                return 'Found a match!'
-        else:
-                return('Not matched!')
-a = input()
-print(text_match(a))
+x=str(input())
+p=re.findall("a(b*)$", x)
+if p:
+    print("Yes")
+else :
+    print ("No")
+
